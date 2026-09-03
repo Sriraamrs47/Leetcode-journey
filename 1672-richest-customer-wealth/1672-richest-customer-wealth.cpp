fbdabad261c7ff538217e37ec80d0ce65ccd0ae2
Sriraam RS <sriraamrs47@gmail.com>
2026-09-03 18:29:39 +0530

    Time: 0 ms (100%), Space: 11.3 MB (81.83%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max_wealth = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            int current_wealth = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                current_wealth = current_wealth + accounts[i][j];
+            }
+            if (current_wealth > max_wealth) {
+                max_wealth = current_wealth;
+            }
+        }
+        return max_wealth;
+    }
+};
